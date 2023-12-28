8faef34eeab49dbf4953720495e09bddd9f5698f
htripathi12 <tripathh@purdue.edu>
2023-12-28 15:28:18 -0500

    figured out multiple animations with intersection observer

--- app.js
+++ app.js
@@ -15,4 +15,6 @@ setTimeout(() => {
 
     const hiddenElements = document.querySelectorAll('.hidden');
     hiddenElements.forEach((e1) => observer.observe(e1));
+    const hiddenElements2 = document.querySelectorAll('.hidden2');
+    hiddenElements2.forEach((e1) => observer.observe(e1));
 }, 3000);
