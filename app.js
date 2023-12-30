6788ce56902e43acc66ab5a1a46700ab1448cf00
htripathi12 <tripathh@purdue.edu>
2023-12-29 22:17:47 -0500

    figured out parallax, final design TBD

--- app.js
+++ app.js
@@ -23,3 +23,14 @@ setTimeout(() => {
     const hiddenElements4 = document.querySelectorAll('.hidden4');
     hiddenElements4.forEach(e1 => observer.observe(e1));
 }, 3000);
+
+document.addEventListener("mousemove", parallax);
+function parallax(event) {
+  this.querySelectorAll(".parallax-wrap span").forEach((shift) => {
+    const position = shift.getAttribute("value");
+    const x = (window.innerWidth - event.pageX * position) / 90;
+    const y = (window.innerHeight - event.pageY * position) / 90;
+
+    shift.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  });
+}
