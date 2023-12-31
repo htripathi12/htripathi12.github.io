70207cdfe66003340b18ec577d13118fc0281e18
htripathi12 <tripathh@purdue.edu>
2023-12-30 23:33:47 -0500

    general code style stuff + TODO: fix menu

--- app.js
+++ app.js
@@ -9,17 +9,20 @@ setTimeout(() => {
             }
         });
     });
-
+    // Slide from left
     const hiddenElements = document.querySelectorAll('.hidden');
     hiddenElements.forEach((e1) => observer.observe(e1));
+    // Slide from right
     const hiddenElements2 = document.querySelectorAll('.hidden2');
     hiddenElements2.forEach((e1) => observer.observe(e1));
+    // Slide from bottom (with increment)
     const hiddenElements3 = document.querySelectorAll('.hidden3');
     hiddenElements3.forEach((e1, index) => {
         setTimeout(() => {
             observer.observe(e1);
         }, index * 250); 
     });
+    // Slide from top
     const hiddenElements4 = document.querySelectorAll('.hidden4');
     hiddenElements4.forEach(e1 => observer.observe(e1));
 }, 3000);
